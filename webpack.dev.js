aef66cafa25209e631f23c2ec123867d7149370c
Joohyun Park <jhp527@nyu.edu>
2021-01-31 21:49:42 -0500

    config mode and open option in devserver

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    port: 3000
+    port: 3000,
+    open: true
   },
   entry: {
     index: './src/index.js'
